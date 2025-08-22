93eddef7ec493155680a7096164832d39eed34f0
Anghelo8 <anghelo.goicochea@vallegrande.edu.pe>
2025-08-22 17:15:26 +0000

    prueba

---
+++ src/main/java/pe/edu/vallegrande/Anghelo/AngheloApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.Anghelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngheloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngheloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/vallegrande/Anghelo/AngheloApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.Anghelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngheloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
